bf4a687bb4cdff85d71edb68cde1df445c5d6e70
Gonçalves Alves <48800908+lucasgoncalvesealves@users.noreply.github.com>
2024-07-09 21:55:54 -0300

    Add files via upload
    Inclusão da página Cadastro.

---
+++ src/pages/cadastro/index.jsx
@@ -0,0 +1,71 @@
+import { useNavigate  } from "react-router-dom";
+import { MdEmail, MdLock, MdPerson } from 'react-icons/md'
+import { Button } from '../../components/Button';
+import { Header } from '../../components/Header';
+import { Input } from '../../components/Input';
+import { api } from '../../services/api';
+
+import { useForm } from "react-hook-form";
+
+
+import { Container, Title, Column, TitleLogin, SubtitleLogin, Row, Wrapper, CriarText } from './styles';
+
+const Cadastro = () => {
+
+    const navigate = useNavigate()
+
+    const { control, handleSubmit, formState: { errors  } } = useForm({
+        reValidateMode: 'onChange',
+        mode: 'onChange',
+    });
+
+    const onSubmit = async (formData) => {
+        try{
+            const {data} = await api.get(`/users?email=${formData.email}&senha=${formData.senha}`);
+            
+            if(data.length && data[0].id){
+                navigate('/feed') 
+                return
+            }
+
+            alert('Usuário ou senha inválido')
+        }catch(e){
+            //TODO: HOUVE UM ERRO
+        }
+    };
+
+    console.log('errors', errors);
+
+    return (<>
+        <Header />
+        <Container>
+            <Column>
+                <Title>A plataforma para você aprender com experts, dominar as principais tecnologias
+                 e entrar mais rápido nas empresas mais desejadas.</Title>
+            </Column>
+            <Column>
+                <Wrapper>
+                <TitleLogin>Comece agora grátis</TitleLogin>
+                <SubtitleLogin>Crie sua conta e make the change._</SubtitleLogin>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <Input placeholder="Nome completo" leftIcon={<MdPerson />} name="nome" control={control} />
+                    {errors.email && <span>Nome completo é obrigatório</span>}
+                    <Input placeholder="E-mail" leftIcon={<MdEmail />} name="email" control={control} />
+                    {errors.email && <span>E-mail é obrigatório</span>}
+                    <Input type="password" placeholder="Senha" leftIcon={<MdLock />} name="senha" control={control} />
+                    {errors.senha && <span>Senha é obrigatório</span>}
+                    <Button title="Criar minha conta" variant="secondary" type="submit"/>
+                </form>
+                <Row>
+                    <p>Ao clicar em "criar minha conta grátis", declaro que aceito as Políticas de Privacidade e os termos de uso da DIO.</p>
+                </Row>
+                <Row>
+                    <CriarText>Já tenho conta. Fazer login.</CriarText>
+                </Row>
+                </Wrapper>
+            </Column>
+        </Container>
+    </>)
+}
+
+export { Cadastro }
